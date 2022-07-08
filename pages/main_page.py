ebf0ca7d8e7c33593be906a694cabdc6a24c012c
ddr533 <ddr533@yandex.ru>
2022-07-08 20:17:44 +0700

    migrated locators from test_main_page.py to locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,10 @@
 from .base_page import BasePage #импорт базового класса
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators #импорт класса с локаторами для главной страницы
+
+
 class MainPage(BasePage): #Создаем новый класс наследник. MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка.
     def go_to_login_page(self): #В аргументы больше не надо передавать экземпляр браузера, мы его передаем и сохраняем на этапе создания Page Object. Вместо него нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класса
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) #символ указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать.
         login_link.click()
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, '#login_linkg'), 'Loging Link is not present'
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Loging Link is not present'
--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,5 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    time.sleep(5)
-    page.should_be_login_link()
+    page.go_to_login_page()  # выполняем метод класса MainPage — переходим на страницу логина
+    page.should_be_login_link() # выполняем метоl класса MainPage - проверяем наличие ссылки на логин
