0f18d6abed941960f1cb4793e0a8921e0652dcdc
ddr533 <ddr533@yandex.ru>
2022-07-11 19:12:18 +0700

    PyTest позволяет объединять несколько тест-кейсов в один класс. C помощью pytest.mark можно помечать сразу весь класс. Основное правило такое: название класса должно начинаться с Test.
    Давайте например объединим в группу два теста в файле test_main_page.py и пометим его меткой login_guest:
    
    @pytest.mark.login_guest
    class TestLoginFromMainPage():
        # не забываем передать первым аргументом self
        def test_guest_can_go_to_login_page(self, browser):
             # реализация теста
    
        def test_guest_should_see_login_link(self, browser):
             # реализация теста
    Попробуйте запустить тесты в этом файле с меткой (нужно добавить "-m login_guest"). Вы увидите, что запустились оба теста

--- test_main_page.py
+++ test_main_page.py
@@ -3,22 +3,22 @@ import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage #для перехода с main_page на login_page
 import time
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем PageObject, передаем в конструктор экземпляра драйвер браузера и url
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод класса MainPage — переходим на страницу логина
-    login_page=LoginPage(browser, browser.current_url) #после перехода создаем экземляр класса LoginPage драйвером браузера и текущим url
-    login_page.should_be_login_page() #выполняем метод из класса
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser): # выполняем метод класса MainPage - проверяем наличие ссылки на логин
-    link = "http://selenium1py.pythonanywhere.com/"
-    page=MainPage(browser,link)
-    page.open()
-    page.should_be_login_link()
-
-
+#@pytest.mark.skip
+@pytest.mark.login_guest #можно запустить только тесты в классе, используя метку -m login_guest
+class TestLoginFromMainPage(): #Объеденили в класс несколько тестов, связанных с одним элементом сайта
+    def test_guest_can_go_to_login_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        self.page = MainPage(browser, link)  # инициализируем PageObject, передаем в конструктор экземпляра драйвер браузера и url
+        self.page.open()  # открываем страницу
+        self.page.go_to_login_page()  # выполняем метод класса MainPage — переходим на страницу логина
+        self.login_page = LoginPage(browser, browser.current_url)  # после перехода создаем экземляр класса LoginPage драйвером браузера и текущим url
+        self.login_page.should_be_login_page()  # выполняем метод из класса
+    def test_guest_should_see_login_link(self,browser):  # выполняем метод класса MainPage - проверяем наличие ссылки на логин
+        link = "http://selenium1py.pythonanywhere.com/"
+        self.page = MainPage(browser, link)
+        self.page.open()
+        self.page.should_be_login_link()
+#@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link='http://selenium1py.pythonanywhere.com/ru/'
     page=MainPage(browser,link)
