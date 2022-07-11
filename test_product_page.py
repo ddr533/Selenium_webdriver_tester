5442f358f56e6a733c291fb785d0a2bd37d1d078
ddr533 <ddr533@yandex.ru>
2022-07-11 14:45:34 +0700

    from test_product_page DELETED page.add_to_basket for passing test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -58,7 +58,6 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page=ProductPage(browser,link)
     page.open()
-    page.add_to_basket()
     page.go_to_basket()
     basket_page=BasketPage(browser,browser.current_url)
     basket_page.user_can_see_message_about_empty_basket()
